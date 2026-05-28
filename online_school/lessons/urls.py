79cf7898a9457f2440ebf8eb588ece38c6aa3315
VFilipev <vasiliy20122009@mail.ru>
2026-05-28 15:15:01 +0500

    создали проект Онлайн Школы и добавли формы для создания урока

--- online_school/lessons/urls.py
+++ online_school/lessons/urls.py
@@ -4,5 +4,5 @@ from .views import lesson_list_view, lesson_create_view
 
 urlpatterns = [
     path('', lesson_list_view, name="home"),
-    path('lessons/create', lesson_create_view, name="lesson-create")
+    path('lessons/create/', lesson_create_view, name="lesson-create")
 ]
